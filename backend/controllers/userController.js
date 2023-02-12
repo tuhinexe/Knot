c6a9f507ab3039ac586ddf7b9e088c66ca815025
Arian Shaikh <ariansk90@gmail.com>
2023-02-13 01:08:23 +0530

    working on: local strategy

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,32 @@
 const express = require("express");
-
-
+const User = require("../models/Users");
+const create_newUser = require("../api/createUser");
+const passport = require("passport");
+passport.use(User.createStrategy());
 const signUpRender = (req, res) => {
   res.render("signUp");
 };
 
 const signUpController = async (req, res) => {
-  console.log("signed up");
-  res.redirect("/login");
+  const userData = {
+    firstname: "raj",
+    lastname: "dk",
+    username: "mehra",
+    email: "random@random.com"
+  }
+  User.register(userData, "blah", (err, user) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/signup');
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        console.log("signed up");
+        res.redirect("/profile");
+      });
+    }
+  });
 };
+ 
 
 const loginRender = (req, res) => {
   res.render("login");
