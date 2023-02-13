d2fb353d0a91e887a9c87040d6650f551a154002
TuhinBar <tuhinbr99@gmail.com>
2023-02-13 14:24:44 +0530

    Revert "Added local strategy"
    This reverts commit b0d9447b5f3fca80423834cf203f8798fe2740e1.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,61 +1,54 @@
-// const express = require("express");
+const express = require("express");
 const User = require("../models/Users");
-const bcrypt = require("bcrypt");
+const passportSetup = require("../config/passportConfig");
 const passport = require("passport");
-// const mongoose = require("mongoose");
-const initializeUser = require("../config/passportLocalConfig"); 
-// const passportLocalMongoose = require("passport-local-mongoose");
 
-initializeUser(passport,email => {
-  return User.find(user => user.email === email)
-},id => {
-  return User.find(user => user.id === id)
-  });
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const signUpRender = (req, res) => {
   res.render("signUp");
 };
 
-//make a local mongoose strategy
-// passport.use(User.createStrategy());
+// make a local mongoose strategy
+passport.use(User.createStrategy());
 
 //serialize and deserialize user
-// passport.serializeUser((user, done) => {
-//   console.log("serialized user");
-//   done(null, user.id);
-// });
+passport.serializeUser((user, done) => {
+  console.log("serialized user");
+  done(null, user.id);
+});
 
-// passport.deserializeUser((id, done) => {
-//   User.findById(id).then((user) => {
-//     done(null, user);
-//   });
-// });
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
-const signUpController = async (req, res) => {
+const signUpController = (req, res) => {
   const userData = {
     firstname: "raj",
     lastname: "dk",
-    username: "hiiiii",
+    username: "heyy",
     email: "random@random.com",
-    password: "ruuu",
   };
-  try{
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
-
-    const user = new User({
-      firstname: userData.firstname,
-      lastname: userData.lastname,
-      username: userData.username,
-      email: userData.email,
-      password: hashedPassword,
-    });
-    await user.save();
-    console.log("hii")
-    res.redirect("/login");
-  }
-  catch{
-    res.redirect("/signup");
-  }
- 
+  User.register(userData, "bhadwa", (err, user) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      console.log("heree");
+      console.log(user.username);
+      passport.authenticate("local", { failureRedirect: "/login" })(
+        req,
+        res,
+        () => {
+          console.log(req.user);
+          res.redirect("/profile");
+        }
+      );
+    }
+  });
 };
 
 const loginRender = (req, res) => {
@@ -63,11 +56,8 @@ const loginRender = (req, res) => {
 };
 
 const loginController = async (req, res) => {
-  passport.authenticate("local", {
-    successRedirect: "/profile",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })(req, res, next);
+  console.log("signed up");
+  res.redirect("/login");
 };
 
 module.exports = {
--- backend/middlewares/authCheck.js
+++
@@ -1,9 +0,0 @@
-const authCheck = (req, res, next) => {
-    if(req.isAuthenticated()){
-        next();
-    } else {
-        res.redirect('/login');
-    }
-}
-
-module.exports = authCheck;
