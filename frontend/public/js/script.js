ca305c44617f1f1385d1a1e1a9c0b9348201dbd5
souvik-wizard <dassouvik3327@gmail.com>
2023-02-13 17:30:55 +0530

    some corrections

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -1,5 +1,10 @@
-document.querySelector("#credentials").addEventListener("click",function(e){
+let togglewithCredentials = document.querySelector("#credentials")
+togglewithCredentials.addEventListener("click", function (e) {
     e.preventDefault()
-    let form=document.querySelector(".hiddenform")
+    let form = document.querySelector(".hiddenform")
     form.classList.toggle("hidden")
+    const formClasses = form.classList;
+
+    Array.from(formClasses).length == 2 ? togglewithCredentials.innerText = "Or, Signup with cedentials." : togglewithCredentials.innerText = "Close Form"
+
 })
