75acd69898bde12f6b2acb08d6ea90ae987edd21
TuhinBar <tuhinbr99@gmail.com>
2023-03-31 20:30:10 +0530

    Update pollsApi.js

--- backend/api/pollsApi.js
+++ backend/api/pollsApi.js
@@ -1,13 +1,9 @@
-const { Console } = require('console');
 const Polls=require('../models/Polls');
 
 const voteCount=async(pollId,optionId,userId)=>{
-
-    console.log(pollId,optionId,userId)
     try{
         const poll=await Polls.findById(pollId);
         let option=poll.options.find(option=>option._id==optionId);
-        console.log(option);
         for(optionCheck of poll.options){
         if(optionCheck.voted_by.includes(userId)){
             return;
