d38a622ccd48701b0f1dc87e1b0a51e6e8a2b117
TuhinBar <tuhinbr99@gmail.com>
2023-03-31 20:24:19 +0530

    Fixed vote bug

--- backend/api/pollsApi.js
+++ backend/api/pollsApi.js
@@ -1,16 +1,23 @@
+const { Console } = require('console');
 const Polls=require('../models/Polls');
 
 const voteCount=async(pollId,optionId,userId)=>{
 
-    const poll=await Polls.findById(pollId);
-    let option=poll.options.find(option=>option._id==optionId);
-    for(option of poll.options){
-        if(option.voted_by.includes(userId)){
+    console.log(pollId,optionId,userId)
+    try{
+        const poll=await Polls.findById(pollId);
+        let option=poll.options.find(option=>option._id==optionId);
+        console.log(option);
+        for(optionCheck of poll.options){
+        if(optionCheck.voted_by.includes(userId)){
             return;
         }
     }
     option.voted_by.push(userId);
     await poll.save();
+    } catch(err){
+        console.log(err);
+    }
      
 
 }
