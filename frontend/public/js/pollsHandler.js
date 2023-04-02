d02ce60a9822a50a34f5f809d0e98f29a3045666
souvik-wizard <dassouvik3327@gmail.com>
2023-04-03 01:47:51 +0530

    polls voted number shown without reload

--- frontend/public/js/pollsHandler.js
+++ frontend/public/js/pollsHandler.js
@@ -3,37 +3,40 @@ import { globalEvent } from "../utils/eventListener.js";
 async function voteCount(e) {
     e.preventDefault();
 
-    const clickedOptionId=e.target.parentElement.getAttribute("data-option-id");
-    const pollId=e.target.parentElement.parentElement.getAttribute("data-poll-id");
-    
+    e.target.parentElement.classList.add("voted");
+    Array.from(e.target.parentElement.parentElement.children).forEach((child) => {
+        child.parentElement.classList.add("pointer-events");
+        child.children[1].classList.remove("hide");
+    })
 
-    try{
+    e.target.parentElement.children[1].innerText = Number(e.target.parentElement.children[1].innerText) + 1;
+
+    const clickedOptionId = e.target.parentElement.getAttribute("data-option-id");
+    const pollId = e.target.parentElement.parentElement.getAttribute("data-poll-id");
+
+    try {
         const response = await fetch("/polls/vote", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({clickedOptionId,pollId}),
+            body: JSON.stringify({ clickedOptionId, pollId }),
 
         });
-     
-        if (response.redirected === true) {
-            window.location = response.url;
-        }
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
 }
 
 let allOptions = document.querySelectorAll(".option");
 allOptions.forEach(function (option) {
-    if(option.classList.contains("voted")){
+    if (option.classList.contains("voted")) {
         option.parentElement.classList.add("pointer-events");
-        Array.from(option.parentElement.children).forEach((child)=>{
+        Array.from(option.parentElement.children).forEach((child) => {
             child.children[1].classList.remove("hide");
         })
     }
 });
 
-globalEvent("click",".option", voteCount);
+globalEvent("click", ".option", voteCount);
