8a0448f7bf6ec1c66a6e969549dcde97ff29bc5e
Arian Shaikh <ariansk90@gmail.com>
2023-04-03 08:28:29 +0530

    removed: the logic to check all the option for voted class and replaced with ejs check

--- backend/models/Polls.js
+++ backend/models/Polls.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const pollsSchema = mongoose.Schema({
     creatorId: { type: mongoose.Schema.Types.ObjectId,ref: "User", required: true },
     content: { type: String, required: true },
+    participators:[{type:mongoose.Schema.Types.ObjectId,ref: "User"}],
     timestamp: { type: Date, default: Date.now } ,
     options: [
         {
