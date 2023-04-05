898e0b2a6f505e9e7b9c97002083c58be19deda9
TuhinBar <tuhinbr99@gmail.com>
2023-04-05 06:55:49 +0530

    Fixed modal fade bug

--- frontend/public/js/editProfileHandler.js
+++ frontend/public/js/editProfileHandler.js
@@ -59,22 +59,3 @@ editProfileSubmit.addEventListener("submit", async (e) => {
   }
 
 });
-
-
-
-const successMessage = document.querySelector(".success-flash");
-
-if (successMessage) {
-  console.log(successMessage);
-  setTimeout(() => {
-    successMessage.style.display = "none";
-  }, 5000);
-}
-
-const errorMessage = document.querySelector(".fail-flash");
-
-if (errorMessage) {
-  setTimeout(() => {
-    errorMessage.style.display = "none";
-  }, 5000 );
-}
