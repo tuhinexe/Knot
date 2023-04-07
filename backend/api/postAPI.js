c6466a668f6b546065d2083094cb093a3d00eb33
Arian Shaikh <ariansk90@gmail.com>
2023-04-07 21:30:04 +0530

    fixed: misc bugs

--- backend/api/postAPI.js
+++ backend/api/postAPI.js
@@ -1,7 +1,7 @@
 const Post = require("../models/Posts");
 const userModel = require("../models/Users");
-const deleteImage = require('./deleteImage');
-const userAPI = require('./userAPI');
+const deleteImage = require("./deleteImage");
+const userAPI = require("./userAPI");
 
 const fetchPosts = async () => {
   const posts = await Post.find({})
@@ -10,7 +10,6 @@ const fetchPosts = async () => {
     .exec();
   return posts;
 };
-
 
 async function findPosts(user) {
   const userFound = await userModel.findById(user._id).populate("posts").exec();
@@ -21,34 +20,37 @@ async function findPosts(user) {
   }
 }
 
-
-const deletePost = async (postId,user) => {
-    const post = await Post.findById(postId);
-    if(post.creator.toString() === user._id.toString()){
-        await Post.findByIdAndDelete(postId);
-        const imageId = await Post.findById(postId).select('imageId');
-        if(imageId !== null){
-            await deleteImage(imageId);
-        }
-        return
-      } else {
-      const foundUser = await userModel.findById(user._id.toString());
-      foundUser.posts.pull(postId);
-      foundUser.points -= 3;
-      await foundUser.save();
-        return;
+const deletePost = async (postId, user) => {
+  const post = await Post.findById(postId);
+  if (post.creator.toString() === user._id.toString()) {
+    await Post.findByIdAndDelete(postId);
+    const imageId = await Post.findById(postId).select("imageId");
+    if (imageId !== null) {
+      await deleteImage(imageId);
     }
-}
+    const foundUser = await userModel.findById(user._id.toString());
+    foundUser.points -= 3;
+    foundUser.posts.pull(postId);
+    await foundUser.save();
 
-const addPost = async (user, content, imageUrl,imageId) => {
-if(!content && !imageUrl) {
-   throw new Error("Please upload an image or write a post");
-}
+    return;
+  } else {
+    const foundUser = await userModel.findById(user._id.toString());
+    foundUser.posts.pull(postId);
+    await foundUser.save();
+    return;
+  }
+};
+
+const addPost = async (user, content, imageUrl, imageId) => {
+  if (!content && !imageUrl) {
+    throw new Error("Please upload an image or write a post");
+  }
   const newPost = new Post({
     content: content,
     creator: user._id,
     imagePath: imageUrl,
-    imageId: imageId
+    imageId: imageId,
   });
   newPost.save((err, post) => {
     if (err) {
@@ -73,17 +75,16 @@ if(!content && !imageUrl) {
   });
 };
 
+const sharePost = async (postId, currentShare, user) => {
+  const post = await Post.findById(postId);
+  if (user.posts.includes(postId)) {
+    return;
+  }
+  const postCreator = await userModel.findById(post.creator);
+  postCreator.points += 2;
+  user.posts.push(postId);
+  post.stats.shares = currentShare + 1;
+  await Promise.all([postCreator.save(), post.save(), user.save()]);
+};
 
-const sharePost = async (postId,currentShare,user) => {
-    const post = await Post.findById(postId);
-    if(user.posts.includes(postId)){
-        return;
-    }
-    const postCreator = await userModel.findById(post.creator);
-    postCreator.points += 2;
-    user.posts.push(postId);
-    post.stats.shares = currentShare+1;
-    await Promise.all([postCreator.save(),post.save(),user.save()])
-}
-
-module.exports = {fetchPosts,findPosts,deletePost,addPost,sharePost}
+module.exports = { fetchPosts, findPosts, deletePost, addPost, sharePost };
